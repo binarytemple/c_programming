1c22a080129264561b9c47042c03318baff02270
bryan hunt <bryan.hunt@picsolve.com>
2013-12-05 18:08:35 +0000

    working with value structs

--- jna_trial/j/CLibraryMain.java
+++ jna_trial/j/CLibraryMain.java
@@ -6,10 +6,12 @@ public class CLibraryMain {
         CLibrary clib = (CLibrary) Native.loadLibrary("testlib", CLibrary.class);
         CLibrary.Example20UnionList.ByReference ex20listref = new CLibrary.Example20UnionList.ByReference();
 
-        ex20listref.numVals = 4;
+        int ARRAY_SIZE = 5;
+
+        ex20listref.numVals = ARRAY_SIZE;
         ex20listref.vals = new CLibrary.Example20UnionHolder.ByReference();
 
-        CLibrary.Example20UnionHolder[] ex20holders = (CLibrary.Example20UnionHolder[])ex20listref.vals.toArray(4);
+        CLibrary.Example20UnionHolder[] ex20holders = (CLibrary.Example20UnionHolder[])ex20listref.vals.toArray(ARRAY_SIZE);
         ex20holders[0].uniontype = CLibrary.Example20UnionType.INTEGER;
         ex20holders[0].unionval.setType(Integer.TYPE);
         ex20holders[0].unionval.intnumber = 31;
@@ -25,6 +27,14 @@ public class CLibraryMain {
         ex20holders[3].uniontype = CLibrary.Example20UnionType.STRING;
         ex20holders[3].unionval.setType(String.class);
         ex20holders[3].unionval.stringval = "hello2";
+
+        ex20holders[4].uniontype = CLibrary.Example20UnionType.STRUCT;
+        ex20holders[4].unionval.setType(CLibrary.ExampleStruct.class);
+        CLibrary.ExampleStruct exampleStruct = new CLibrary.ExampleStruct();
+        exampleStruct.name = "foo";
+        exampleStruct.x = 10;
+        exampleStruct.write();
+        ex20holders[4].unionval.structval = exampleStruct;
 
         ex20listref.vals.write();
 
